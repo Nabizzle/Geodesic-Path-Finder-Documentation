d7e556d47a13a704fa1d05d9873f0ce7fbd7e166
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2024-02-14 23:18:56 -0500

    docs: Adds the sphinx files for data_manager

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,35 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+import os
+import sys
 
-release = '0.1'
-version = '0.1.0'
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'Code')))
 
-# -- General configuration
+project = 'Drawing to 3D'
+copyright = '2024, Nabeel Chowdhury'
+author = 'Nabeel Chowdhury'
+release = 'v1.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
---
+++ docs/source/drawingto3D.rst
@@ -0,0 +1,21 @@
+drawingto3D package
+===================
+
+Submodules
+----------
+
+drawingto3D.data\_manager module
+--------------------------------
+
+.. automodule:: drawingto3D.data_manager
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: drawingto3D
+   :members:
+   :undoc-members:
+   :show-inheritance:
